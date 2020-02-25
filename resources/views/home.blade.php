32e6c36bb9aa8801fb4b377da068be96bbb6c802
AmujoDotun <samuelamujo@gmail.com>
2020-02-25 12:10:55 +0100

    working on the dashboard when login, done with the user profile picture with the follower post and following all with the address and link to officail website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="row">
+        <div class="col-3 p-5">
+         <img src="/img/slack-customer-rmit.jpg" width="100" height="100" class="rounded-circle">
+    </div> 
+        <div class="col-9 pt-5">
+            <div><h1>DotSocial</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>150</strong> post</div>
+                    <div class="pr-5"><strong>250k</strong> follower</div>
+                    <div class="pr-5"><strong>150</strong> following</div>
                 </div>
+                <div class="pt-5 font-weight-bold">Dotsocial.org</div>
+                <div>We are the best you need for a lifetime......oh yes......you heard it right</div>
+                <div><a href="#">DotSocial.com</a></div>
             </div>
         </div>
-    </div>
 </div>
 @endsection
