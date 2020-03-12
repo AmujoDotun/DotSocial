fdbee134e32b56b35199940b8d7b35d0deef7691
AmujoDotun <samuelamujo@gmail.com>
2020-03-12 14:07:36 +0100

    set a default image for new users that didn't have profile picture

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded =[];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : '/profile\wkqSdCJVuZK7svu1p4xhA74xTXh8KMmTVZZG0tKx.jpeg';
+        return '/storage/' . $imagePath;
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
