1b7fb3d24119de6a5be00c78b1b60813f7725b89
AmujoDotun <samuelamujo@gmail.com>
2020-03-12 14:08:56 +0100

    added a comment for the profileImage function

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,7 @@ class Profile extends Model
 {
     protected $guarded =[];
 
+    //this function is to set a default image or retain existing profile picture
     public function profileImage(){
         $imagePath = ($this->image) ? $this->image : '/profile\wkqSdCJVuZK7svu1p4xhA74xTXh8KMmTVZZG0tKx.jpeg';
         return '/storage/' . $imagePath;
