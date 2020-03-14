1ea67032fbdee0640294afab455fd0b0add06223
AmujoDotun <samuelamujo@gmail.com>
2020-03-14 17:52:07 +0100

    created follow user method and passed the props of userId to trigger the follow button on the user that is following who

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button class="btn btn-primary ml-4" @click="FollowUser">Follow</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['userId'],
+
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods: {
+            
+
+            FollowUser(){
+                // make a post request to this endpoint
+            axios.post('/follow/' + this.userId)
+             .then(response=>{
+                 console.log(response.data);
+             });
+            }
+        }
+    }
+</script>
